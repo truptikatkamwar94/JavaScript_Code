85bf698ca67ab12cf78ef768d27ee0a68553ac6a
trupti <truptikatkamwar94@gmail.com>
2024-02-24 19:33:32 +0530

    Data-types

---
+++ data_types.js
@@ -0,0 +1,13 @@
+let age = 22;
+console.log(typeof(age))
+let price =100.9
+console.log(typeof(price))
+let name = "Arijit"
+console.log(typeof(name))
+isFollow = true;
+console.log(typeof(isFollow))
+let x;
+console.log(typeof(x))
+
+let null_var = null
+console.log(typeof(null_var))
