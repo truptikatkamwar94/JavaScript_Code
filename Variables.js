94110a9a1501536bfaac4eeaf8c6686099db7f28
trupti <truptikatkamwar94@gmail.com>
2024-02-24 19:02:03 +0530

    js-variables

---
+++ Variables.js
@@ -0,0 +1,25 @@
+// var
+var age = 24
+var age = 23
+console.log("var variable==>",age)
+
+//let(modern JS)
+let age_1 = 22
+age_1 = 23
+console.log("let variable==>",age_1)
+let a;
+console.log("Undefined variables==>",a)
+{
+    let a = 100
+    console.log("Block level==>",a)
+}
+{
+
+    let a = 200
+    console.log("Block level==>",a)
+}
+
+//const
+const age_2 = 23
+age_2 = 234
+console.log("Consant variables==>",age_2)
